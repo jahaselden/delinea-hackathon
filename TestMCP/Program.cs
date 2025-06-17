246b89efd76fff3b94ba61ac559a4d8294a4a654
jen.haselden <jen.haselden@delinea.com>
2025-06-17 11:44:03 -0600

    initial commit

---
+++ TestMCP/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Logging.AddConsole(consoleLogOptions =>
+{
+    // Configure all logs to go to stderr
+    consoleLogOptions.LogToStandardErrorThreshold = LogLevel.Trace;
+});
+builder.Services
+    .AddMcpServer()
+    .WithStdioServerTransport()
+    .WithToolsFromAssembly();
+await builder.Build().RunAsync();
+
+[McpServerToolType] //tool types that are exposed to mcp - three currently
+public static class EchoTool
+{
+    [McpServerTool, Description("Echoes the message back to the client.")]
+    public static string Echo(string message) => $"hello {message}";
+
+    [McpServerTool, Description("Echoes back the message in reverse.")]
+    public static string Reverse(string message) => new string(message.Reverse().ToArray());
+
+    [McpServerTool, Description("Echoes back the length of the message.")]
+    public static int Length(string message) => message.Length;
+}
